a0153036fc9d92620e9378f49318463f8a91134f
Portocash <76227995+Portocash@users.noreply.github.com>
2023-10-29 18:06:12 +0100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILENAME="results/2550_3_dk.csv"
-
-vmstat 1 300 | awk '{if (NR>2) { "date +\"%Y-%m-%d %H:%M:%S\"" | getline timestamp; print timestamp "," $0; } }' | tr -s ' ' ',' > "$FILENAME" &
-
-/usr/sbin/apache2ctl -D FOREGROUND
-
